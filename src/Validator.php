73ba69eebd193e68d454159a5de4f7ebbac785c7
Andreas Bratfisch <duskman72@gmail.com>
2016-01-06 13:48:28 +0100

    Fix class not found

--- src/Validator.php
+++ src/Validator.php
@@ -25,7 +25,7 @@ trait Validator {
             // split validation rule into required / sometimes (no validation if not set or empty)
             // contract and contract parameters
             $parts = explode("|", $rule);
-            if (is_null($parts) || !is_array($parts) || count($parts) < 3)
+            if (is_null($parts) || !is_array($parts) || count($parts) <2)
                 throw new ValidationException("Invalid rule");
 
             $required = strtolower($parts[0]) == "required" ? true : false;
@@ -35,7 +35,7 @@ trait Validator {
             }
 
             $args = explode(":", $parts[1]);
-            $clazz = Inflector::classify($args[0])."Contract";
+            $clazz = __NAMESPACE__. "\\Contracts\\" . Inflector::classify($args[0])."Contract";
             if (!class_exists($clazz))
                 throw new ValidationException("Invalid rule: invalid validation class '$clazz'");
 
